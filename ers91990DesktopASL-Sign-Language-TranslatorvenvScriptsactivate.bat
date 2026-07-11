f6f09ddfa008fe43cc3f80bebefa1a016b16cbc7
Anonymous-avi <clownalphareal11@gmail.com>
2026-07-11 16:01:02 +0530

    Added dataset and model support for M N and O

---
+++ ers91990DesktopASL-Sign-Language-TranslatorvenvScriptsactivate.bat
@@ -0,0 +1,27 @@
+078104e | Anonymous-avi | clownalphareal11@gmail.com | Improved dataset for A, B and C
+9f593eb | HarshitaGupta-610 | harshitagupta.ambala@gmail.com | Updated dataset for D, E and F
+87cb3e5 | HarshitaGupta-610 | harshitagupta.ambala@gmail.com | Finalized Tkinter ASL application with live prediction
+dd181d0 | HarshitaGupta-610 | harshitagupta.ambala@gmail.com | Merge branch 'avika-dev' of https://github.com/HarshitaGupta-610/ASL-Sign-Language-Translator into harshita-dev
+df40d89 | Anonymous-avi | clownalphareal11@gmail.com | Added the live video generation part via webrtc
+87f84da | HarshitaGupta-610 | harshitagupta.ambala@gmail.com | Merge branch 'avika-dev' of https://github.com/HarshitaGupta-610/ASL-Sign-Language-Translator into harshita-dev
+207c618 | Anonymous-avi | clownalphareal11@gmail.com | Merge branch 'harshita-dev' of https://github.com/HarshitaGupta-610/ASL-Sign-Language-Translator into avika-dev
+f3dd4a0 | HarshitaGupta-610 | harshitagupta.ambala@gmail.com | Day 7: Added Streamlit frontend UI
+03d1e7b | Anonymous-avi | clownalphareal11@gmail.com | Accuracy report added
+9d3fb31 | Anonymous-avi | clownalphareal11@gmail.com | Completed Day 7 documentation and demo preparation
+b1db0be | Anonymous-avi | clownalphareal11@gmail.com | Completed Day 6 testing and evaluation
+d96a60c | HarshitaGupta-610 | harshitagupta.ambala@gmail.com | Expanded model to support A-F signs
+9dd5707 | Anonymous-avi | clownalphareal11@gmail.com | Implemented live ASL prediction system
+9c44a48 | Anonymous-avi | clownalphareal11@gmail.com | Completed Day 4 model evaluation
+be5fb72 | Anonymous-avi | clownalphareal11@gmail.com | Completed Day 3 dataset analysis and EDA
+d8e3c09 | HarshitaGupta-610 | harshitagupta.ambala@gmail.com | Trained Random Forest model and evaluated performance
+644c4a8 | HarshitaGupta-610 | harshitagupta.ambala@gmail.com | Fixed preprocessing notes
+6e782be | HarshitaGupta-610 | harshitagupta.ambala@gmail.com | Completed preprocessing and train test split
+e237fd4 | Anonymous-avi | clownalphareal11@gmail.com | Added dataset validation and analysis tools
+985c612 | HarshitaGupta-610 | harshitagupta.ambala@gmail.com | Added dataset collection pipeline and collected initial dataset
+f643264 | Anonymous-avi | clownalphareal11@gmail.com | Added ASL research and landmark visualizer
+8c54bc6 | HarshitaGupta-610 | harshitagupta.ambala@gmail.com | Added sample saving functionality
+adde7c0 | HarshitaGupta-610 | harshitagupta.ambala@gmail.com | Added MediaPipe documentation
+6b6368b | HarshitaGupta-610 | harshitagupta.ambala@gmail.com | Added landmark extraction
+db445cc | HarshitaGupta-610 | harshitagupta.ambala@gmail.com | Added webcam test
+3421ffb | HarshitaGupta-610 | harshitagupta.ambala@gmail.com | Initial project structure
+d7c069a | Harshita Gupta | harshitagupta.ambala@gmail.com | Initial commit
